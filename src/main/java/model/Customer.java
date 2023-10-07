f3e0313bb47e54328f90b227b9c6b09ba9194e9b
Shahrad <shahradbagheri6@gmail.com>
2023-10-07 08:18:44 +0330

    added fields to Customer

--- src/main/java/model/Customer.java
+++ src/main/java/model/Customer.java
@@ -1,4 +1,14 @@
 package model;
 
+import javax.persistence.Entity;
+import java.time.LocalDate;
+
+@Entity
 public class Customer extends User{
+
+    private String firstname;
+
+    private String lastname;
+
+    private LocalDate registrationDate;
 }
