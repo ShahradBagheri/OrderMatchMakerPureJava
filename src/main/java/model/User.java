b19a270d1d08dcd537d53196b130e2c1c17c4388
Shahrad <shahradbagheri6@gmail.com>
2023-10-07 06:50:06 +0330

    added fields

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,4 +1,16 @@
 package model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
 public abstract class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+
+    private String password;
 }
