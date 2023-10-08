389c4863b4169d404e15f93a20ef2d9bbf45671a
Shahrad <shahradbagheri6@gmail.com>
2023-10-08 08:15:25 +0330

    created Expert

--- src/main/java/model/Expert.java
+++ src/main/java/model/Expert.java
@@ -1,7 +1,24 @@
 package model;
 
+import model.enumeration.ExpertStatus;
+
 import javax.persistence.Entity;
+import javax.persistence.PrePersist;
+import java.time.LocalDate;
 
 @Entity
 public class Expert extends User{
+
+    private String firstname;
+
+    private String lastname;
+
+    private ExpertStatus expertStatus;
+
+    private LocalDate registrationDate;
+
+    @PrePersist
+    private void onCreate(){
+        registrationDate = LocalDate.now();
+    }
 }
