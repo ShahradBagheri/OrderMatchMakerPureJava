eee69211557b1e3593582b6d5ea68b9663ad7945
Shahrad <shahradbagheri6@gmail.com>
2023-10-09 19:41:15 +0330

    created OrderState enum

---
+++ src/main/java/model/enumeration/OrderState.java
@@ -0,0 +1,10 @@
+package model.enumeration;
+
+public enum OrderState {
+    WAITING_FOR_SUGGESTIONS,
+    WAITING_TO_SELECT_SUGGESTION,
+    WAITING_FOR_EXPERT,
+    STARTED,
+    FINISHED,
+    PAID
+}
