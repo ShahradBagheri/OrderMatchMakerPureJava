4e6bb916c5f68699c27746ca49d02786eba48607
Shahrad <shahradbagheri6@gmail.com>
2023-10-09 19:14:12 +0330

    added oneToMany relation between MainService and SubService

--- src/main/java/model/MainService.java
+++ src/main/java/model/MainService.java
@@ -1,4 +1,20 @@
 package model;
 
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Data
+@Entity
 public class MainService {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @OneToMany
+    private List<SubService> subServices;
 }
--- src/main/java/model/SubService.java
+++ src/main/java/model/SubService.java
@@ -1,17 +1,19 @@
 package model;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToMany;
-import java.util.List;
+import lombok.Data;
 
+import javax.persistence.*;
+
+@Data
+@Entity
 public class SubService {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToMany
-    private List<SubService> subServices;
+    private String name;
+
+    @ManyToOne
+    private SubService subServices;
 }
