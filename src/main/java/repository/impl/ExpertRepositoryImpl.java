9e39281cab30d7deaaf4a589d3964c0af5810863
Shahrad <shahradbagheri6@gmail.com>
2023-10-11 15:26:30 +0330

    created ExpertRepositoryImpl

---
+++ src/main/java/repository/impl/ExpertRepositoryImpl.java
@@ -0,0 +1,33 @@
+package repository.impl;
+
+import model.Expert;
+
+import java.util.List;
+
+public class ExpertRepositoryImpl implements repository.ExpertRepository {
+
+    @Override
+    public Expert create(Expert expert) {
+        return null;
+    }
+
+    @Override
+    public Expert update(Expert expert) {
+        return null;
+    }
+
+    @Override
+    public void delete(Expert expert) {
+
+    }
+
+    @Override
+    public Expert findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<Expert> findAll() {
+        return null;
+    }
+}
