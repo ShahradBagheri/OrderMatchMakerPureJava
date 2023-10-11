b9d7e3c723af7adadbd318061c602b3b6f697501
Shahrad <shahradbagheri6@gmail.com>
2023-10-11 15:32:55 +0330

    added methods to the interface

--- src/main/java/repository/MainServiceRepository.java
+++ src/main/java/repository/MainServiceRepository.java
@@ -1,4 +1,18 @@
 package repository;
 
-public class MainServiceRepository {
+import model.MainService;
+
+import java.util.List;
+
+public interface MainServiceRepository {
+
+    MainService create(MainService mainService);
+
+    MainService update(MainService mainService);
+
+    void delete(MainService mainService);
+
+    MainService findById(Long id);
+
+    List<MainService> findAll();
 }
