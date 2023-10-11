3215c87874588906fb3e2fcebabaaf60bc6cabdf
Shahrad <shahradbagheri6@gmail.com>
2023-10-11 15:23:50 +0330

    added methods to the interface

--- src/main/java/repository/ExpertRepository.java
+++ src/main/java/repository/ExpertRepository.java
@@ -1,4 +1,18 @@
 package repository;
 
-public class ExpertRepository {
+import model.Expert;
+
+import java.util.List;
+
+public interface ExpertRepository {
+
+    Expert create(Expert expert);
+
+    Expert update(Expert expert);
+
+    void delete(Expert expert);
+
+    Expert findById(Long id);
+
+    List<Expert> findAll();
 }
