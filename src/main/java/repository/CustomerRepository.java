91c4c3232a998a21a733180f8692133983b59d8a
Shahrad <shahradbagheri6@gmail.com>
2023-10-11 15:21:10 +0330

    added methods to CustomerRepository

--- src/main/java/repository/CustomerRepository.java
+++ src/main/java/repository/CustomerRepository.java
@@ -1,4 +1,18 @@
 package repository;
 
-public class CustomerRepository {
+import model.Customer;
+
+import java.util.List;
+
+public interface CustomerRepository {
+
+    Customer create(Customer customer);
+
+    Customer update(Customer customer);
+
+    void delete(Customer customer);
+
+    Customer findById(Long id);
+
+    List<Customer> findAll();
 }
