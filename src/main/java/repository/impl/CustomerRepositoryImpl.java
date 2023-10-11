2a6234f954d883e9f1129b3d2ab8d885be1087c8
Shahrad <shahradbagheri6@gmail.com>
2023-10-11 15:21:27 +0330

    created and implemented CustomerRepositoryImpl

---
+++ src/main/java/repository/impl/CustomerRepositoryImpl.java
@@ -0,0 +1,43 @@
+package repository.impl;
+
+import model.Customer;
+import repository.CustomerRepository;
+import util.EntityManagerSingleton;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+public class CustomerRepositoryImpl implements CustomerRepository {
+
+    private final EntityManager entityManager = EntityManagerSingleton.getInstanceEM();
+
+    @Override
+    public Customer create(Customer customer) {
+        entityManager.persist(customer);
+        return customer;
+    }
+
+    @Override
+    public Customer update(Customer customer) {
+        entityManager.merge(customer);
+        return customer;
+    }
+
+    @Override
+    public void delete(Customer customer) {
+        entityManager.detach(customer);
+    }
+
+    @Override
+    public Customer findById(Long id) {
+        return entityManager.find(Customer.class,id);
+    }
+
+    @Override
+    public List<Customer> findAll() {
+        String jpql = "SELECT c FROM Customer c";
+        TypedQuery<Customer> typedQuery = entityManager.createQuery(jpql, Customer.class);
+        return typedQuery.getResultList();
+    }
+}
