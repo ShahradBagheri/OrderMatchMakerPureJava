465730b13c86274455438ca009b112d31f05ffde
Shahrad <shahradbagheri6@gmail.com>
2023-10-12 20:15:23 +0330

    added SubServiceService methods

--- src/main/java/service/SubServiceService.java
+++ src/main/java/service/SubServiceService.java
@@ -1,4 +1,18 @@
 package service;
 
+import model.MainService;
+
+import java.util.List;
+
 public interface SubServiceService {
+
+    MainService create(MainService mainService);
+
+    MainService update(MainService mainService);
+
+    void delete(MainService mainService);
+
+    MainService findById(Long id);
+
+    List<MainService> findAll();
 }
