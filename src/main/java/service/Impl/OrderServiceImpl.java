4994d6414eb9814cce63d1812ec631278de38bef
Shahrad <shahradbagheri6@gmail.com>
2023-10-12 21:37:57 +0330

    implemented the findAll method

--- src/main/java/service/Impl/OrderServiceImpl.java
+++ src/main/java/service/Impl/OrderServiceImpl.java
@@ -1,5 +1,6 @@
 package service.Impl;
 
+import model.MainService;
 import model.Order;
 import repository.OrderRepository;
 import service.OrderService;
@@ -7,6 +8,8 @@ import util.ApplicationContext;
 import util.EntityManagerSingleton;
 
 import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.PersistenceException;
 import java.util.List;
 
 public class OrderServiceImpl implements OrderService {
@@ -36,6 +39,20 @@ public class OrderServiceImpl implements OrderService {
 
     @Override
     public List<Order> findAll() {
-        return null;
+        EntityTransaction entityTransaction = entityManager.getTransaction();
+
+        try {
+            entityTransaction.begin();
+
+            List<Order> orders = orderRepository.findAll();
+
+            entityTransaction.commit();
+            return orders;
+
+        } catch (PersistenceException | IllegalStateException e) {
+
+            entityTransaction.rollback();
+            return null;
+        }
     }
 }
