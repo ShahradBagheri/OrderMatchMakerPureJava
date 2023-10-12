7c5c0d66bf759d5555054ef04b7a4f32132ee06b
Shahrad <shahradbagheri6@gmail.com>
2023-10-12 21:47:56 +0330

    added unique to name fields

--- src/main/java/model/MainService.java
+++ src/main/java/model/MainService.java
@@ -17,6 +17,7 @@ public class MainService {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(unique = true)
     private String name;
 
     @OneToMany(mappedBy = "mainService")
--- src/main/java/model/SubService.java
+++ src/main/java/model/SubService.java
@@ -16,6 +16,7 @@ public class SubService {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(unique = true)
     private String name;
 
     private Double basePrice;
