534e9c44243ae0cc5b8c993c2a540f636aead5d8
Shahrad <shahradbagheri6@gmail.com>
2023-10-12 23:10:02 +0330

    added customer tests in main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,23 +7,31 @@ import util.ApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
-        //create customer
-        User user = User.builder()
-                .firstname("Shahrad")
-                .lastname("Bagheri")
-                .email("shahrad@gmail.com")
-                .password("something")
-                .role(Role.CUSTOMER)
-                .build();
 
-        Customer customer = new Customer();
-        customer.setUser(user);
-        customer = ApplicationContext.customerService.create(customer);
+        //create customer
+//        User user = User.builder()
+//                .firstname("Shahrad")
+//                .lastname("Bagheri")
+//                .email("shahrad@gmail.com")
+//                .password("something")
+//                .role(Role.CUSTOMER)
+//                .build();
+//
+//        Customer customer = new Customer();
+//        customer.setUser(user);
+//        customer = ApplicationContext.customerService.create(customer);
 
         //update customer
-//
 //        Customer customer = ApplicationContext.customerService.findById(1L);
-//        customer.setUser(user);
+//        customer.getUser().setFirstname("changed");
+//        customer.getUser().setLastname("changed");
 //        ApplicationContext.customerService.update(customer);
+
+        //find all customer
+//        System.out.println(ApplicationContext.customerService.findAll());
+
+        //delete customer
+//        ApplicationContext.customerService.delete(ApplicationContext.customerService.findById(1L));
+
     }
 }
