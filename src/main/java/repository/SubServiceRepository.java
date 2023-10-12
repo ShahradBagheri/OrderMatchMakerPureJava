f6619111d9f8c471fc8739558a77b77112e94727
Shahrad <shahradbagheri6@gmail.com>
2023-10-12 08:11:44 +0330

    added methods to SubServiceRepository

--- src/main/java/repository/SubServiceRepository.java
+++ src/main/java/repository/SubServiceRepository.java
@@ -1,4 +1,18 @@
 package repository;
 
+import model.SubService;
+
+import java.util.List;
+
 public interface SubServiceRepository {
+
+    SubService create(SubService subService);
+
+    SubService update(SubService subService);
+
+    void delete(SubService subService);
+
+    SubService findById(SubService id);
+
+    List<SubService> findAll();
 }
