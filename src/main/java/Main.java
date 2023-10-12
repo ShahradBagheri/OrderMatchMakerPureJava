13632b20181bcd967edd9c2a4f50be06df7292ee
Shahrad <shahradbagheri6@gmail.com>
2023-10-13 01:22:27 +0330

    added tests to main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,12 @@
-import model.Customer;
-import model.Expert;
-import model.User;
+import model.*;
 import model.enumeration.Role;
 import service.CustomerService;
 import service.Impl.CustomerServiceImpl;
+import service.UserService;
 import util.ApplicationContext;
 import util.ImageProcessing;
+
+import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
@@ -47,9 +48,14 @@ public class Main {
 //                .firstname("Shahrad")
 //                .lastname("Bagheri")
 //                .email("shahrad2@gmail.com")
-//                .password("something")
+//                .password("something1")
 //                .role(Role.EXPERT)
 //                .build();
+//
+//        System.out.println(ApplicationContext.userService.isValidEmail(user.getEmail()));
+//        System.out.println(ApplicationContext.userService.isValidPassword(user.getPassword()));
+
+//        System.out.println(ImageProcessing.validImageSize("C:\\Users\\Shahrad\\IdeaProjects\\PureJavaProject\\src\\main\\resources\\image\\picture.png"));
 //
 //        Expert expert = Expert.builder()
 //                .user(user)
@@ -73,5 +79,81 @@ public class Main {
 //        ApplicationContext.expertService.delete(ApplicationContext.expertService.findById(1L));
 
 
+
+
+        //MainService create
+//
+//        MainService mainService = MainService.builder()
+//                .name("something")
+//                .build();
+//
+//        ApplicationContext.mainServiceService.create(mainService);
+
+
+        //MainService update
+
+//        MainService mainService = ApplicationContext.mainServiceService.findById(1L);
+//        mainService.setName("changed");
+//
+//        ApplicationContext.mainServiceService.update(mainService);
+
+        //MainService findAll
+
+//        System.out.println(ApplicationContext.mainServiceService.findAll());
+
+        //MainService Delete
+
+//        ApplicationContext.mainServiceService.delete(ApplicationContext.mainServiceService.findById(1L));
+
+
+
+
+
+        //subService create
+//        SubService subService = SubService.builder()
+//                .name("some name")
+//                .basePrice(3000.0)
+//                .comment("info")
+//                .mainService(ApplicationContext.mainServiceService.findById(2L))
+//                .build();
+//
+//        ApplicationContext.subServiceService.update(subService);
+
+        //subService update
+
+//        SubService subService = ApplicationContext.subServiceService.findById(1L);
+//        subService.setName("Changed");
+//        ApplicationContext.subServiceService.update(subService);
+
+        //subService findAll
+
+//        System.out.println(ApplicationContext.subServiceService.findAll());
+
+        //subService delete
+//        ApplicationContext.subServiceService.delete(ApplicationContext.subServiceService.findById(1L));
+
+        //order create
+
+//        Order order = Order.builder()
+//                .subService(ApplicationContext.subServiceService.findById(1L))
+//                .customer(ApplicationContext.customerService.findById(1L))
+//                .suggestedPrice(2000.0)
+//                .details("some details")
+//                .startingDate(LocalDate.of(2024,12,12))
+//                .build();
+
+//        ApplicationContext.orderService.validPrice(order);
+//
+//        ApplicationContext.orderService.create(order);
+
+        //order update
+
+//        Order order = ApplicationContext.orderService.findById(3L);
+//        order.setExpert(ApplicationContext.expertService.findById(1L));
+//        order.setAddress("something");
+//        ApplicationContext.orderService.update(order);
+
+        //order delete
+//        ApplicationContext.subServiceService.delete(ApplicationContext.subServiceService.findById(1L));
     }
 }
