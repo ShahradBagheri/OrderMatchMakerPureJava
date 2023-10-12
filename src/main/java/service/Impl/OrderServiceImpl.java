3042d1644cfefd9c6d8b8da8776df4bfa3219b43
Shahrad <shahradbagheri6@gmail.com>
2023-10-12 21:37:16 +0330

    added fields and methods

--- src/main/java/service/Impl/OrderServiceImpl.java
+++ src/main/java/service/Impl/OrderServiceImpl.java
@@ -1,4 +1,41 @@
 package service.Impl;
 
-public class OrderServiceImpl {
+import model.Order;
+import repository.OrderRepository;
+import service.OrderService;
+import util.ApplicationContext;
+import util.EntityManagerSingleton;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+public class OrderServiceImpl implements OrderService {
+
+    private final EntityManager entityManager = EntityManagerSingleton.getInstanceEM();
+    private final OrderRepository orderRepository = ApplicationContext.orderRepository;
+
+    @Override
+    public Order create(Order order) {
+        return null;
+    }
+
+    @Override
+    public Order update(Order order) {
+        return null;
+    }
+
+    @Override
+    public void delete(Order order) {
+
+    }
+
+    @Override
+    public Order findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<Order> findAll() {
+        return null;
+    }
 }
