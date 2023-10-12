1e40728438f7548920924c8dbebb9c3872e4d938
Shahrad <shahradbagheri6@gmail.com>
2023-10-12 08:10:08 +0330

    added methods to OfferRepository

--- src/main/java/repository/OfferRepository.java
+++ src/main/java/repository/OfferRepository.java
@@ -1,4 +1,18 @@
 package repository;
 
+import model.Offer;
+
+import java.util.List;
+
 public interface OfferRepository {
+
+    Offer create(Offer offer);
+
+    Offer update(Offer offer);
+
+    void delete(Offer offer);
+
+    Offer findById(Long id);
+
+    List<Offer> findAll();
 }
