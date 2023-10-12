a0096d6be878a09fe0b578bbf59ecd6416a299ea
Shahrad <shahradbagheri6@gmail.com>
2023-10-12 15:14:29 +0330

    added methods to the interface

--- src/main/java/service/Impl/MainServiceServiceImpl.java
+++ src/main/java/service/Impl/MainServiceServiceImpl.java
@@ -1,4 +1,34 @@
 package service.Impl;
 
-public class MainServiceServiceImpl {
+import model.MainService;
+import service.MainServiceService;
+
+import java.util.List;
+
+public class MainServiceServiceImpl implements MainServiceService {
+
+    @Override
+    public MainService create(MainService mainService) {
+        return null;
+    }
+
+    @Override
+    public MainService update(MainService mainService) {
+        return null;
+    }
+
+    @Override
+    public void delete(MainService mainService) {
+
+    }
+
+    @Override
+    public MainService findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<MainService> findAll() {
+        return null;
+    }
 }
