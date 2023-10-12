0e2ad208e75787d4090df23e41beafbfbcd57e0d
Shahrad <shahradbagheri6@gmail.com>
2023-10-12 20:19:33 +0330

    implemented the create method

--- src/main/java/service/Impl/SubServiceServiceImpl.java
+++ src/main/java/service/Impl/SubServiceServiceImpl.java
@@ -1,4 +1,56 @@
 package service.Impl;
 
-public class SubServiceServiceImpl {
+import model.SubService;
+import repository.SubServiceRepository;
+import service.SubServiceService;
+import util.ApplicationContext;
+import util.EntityManagerSingleton;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.PersistenceException;
+import java.util.List;
+
+public class SubServiceServiceImpl implements SubServiceService {
+
+    private final SubServiceRepository subServiceRepository = ApplicationContext.subServiceRepository;
+    private final EntityManager entityManager = EntityManagerSingleton.getInstanceEM();
+
+    @Override
+    public SubService create(SubService subService) {
+        EntityTransaction entityTransaction = entityManager.getTransaction();
+
+        try {
+            entityTransaction.begin();
+
+            subServiceRepository.create(subService);
+
+            entityTransaction.commit();
+            return subService;
+        } catch (PersistenceException | IllegalStateException e) {
+
+            entityTransaction.rollback();
+            return null;
+        }
+    }
+
+    @Override
+    public SubService update(SubService subService) {
+        return null;
+    }
+
+    @Override
+    public void delete(SubService subService) {
+
+    }
+
+    @Override
+    public SubService findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<SubService> findAll() {
+        return null;
+    }
 }
