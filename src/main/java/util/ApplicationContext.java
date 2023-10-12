3a9d804b4cf03988c9164ab11654ecb3a60282ea
Shahrad <shahradbagheri6@gmail.com>
2023-10-12 22:25:42 +0330

    added services to ApplicationContext

--- src/main/java/util/ApplicationContext.java
+++ src/main/java/util/ApplicationContext.java
@@ -1,7 +1,11 @@
 package util;
 
+import model.MainService;
+import model.SubService;
 import repository.*;
 import repository.impl.*;
+import service.*;
+import service.Impl.*;
 
 public class ApplicationContext {
 
@@ -13,6 +17,13 @@ public class ApplicationContext {
     public static final OrderRepository orderRepository;
     public static final SubServiceRepository subServiceRepository;
 
+    public static final UserService userService;
+    public static final CustomerService customerService;
+    public static final ExpertService expertService;
+    public static final MainServiceService mainServiceService;
+    public static final OrderService orderService;
+    public static final SubServiceService subServiceService;
+
     static {
         userRepository = new UserRepositoryImpl();
         customerRepository = new CustomerRepositoryImpl();
@@ -21,5 +32,12 @@ public class ApplicationContext {
         offerRepository = new OfferRepositoryImpl();
         orderRepository = new OrderRepositoryImpl();
         subServiceRepository = new SubServiceRepositoryImpl();
+
+        userService = new UserServiceImpl();
+        customerService = new CustomerServiceImpl();
+        expertService = new ExpertServiceImpl();
+        mainServiceService = new MainServiceServiceImpl();
+        orderService = new OrderServiceImpl();
+        subServiceService = new SubServiceServiceImpl();
     }
 }
