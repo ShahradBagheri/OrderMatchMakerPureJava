fc562554de4687043f9346747d81523d9a4272ff
Shahrad <shahradbagheri6@gmail.com>
2023-10-12 08:47:44 +0330

    created ApplicationContext

--- src/main/java/util/ApplicationContext.java
+++ src/main/java/util/ApplicationContext.java
@@ -1,4 +1,23 @@
 package util;
 
+import repository.*;
+import repository.impl.*;
+
 public class ApplicationContext {
+
+    public static final CustomerRepository customerRepository;
+    public static final ExpertRepository expertRepository;
+    public static final MainServiceRepository mainServiceRepository;
+    public static final OfferRepository offerRepository;
+    public static final OrderRepository orderRepository;
+    public static final SubServiceRepository subServiceRepository;
+
+    static {
+        customerRepository = new CustomerRepositoryImpl();
+        expertRepository = new ExpertRepositoryImpl();
+        mainServiceRepository = new MainServiceImpl();
+        offerRepository = new OfferRepositoryImpl();
+        orderRepository = new OrderRepositoryImpl();
+        subServiceRepository = new SubServiceRepositoryImpl();
+    }
 }
