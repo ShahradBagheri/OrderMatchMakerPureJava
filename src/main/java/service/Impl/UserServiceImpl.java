d15f46ae547e22ea75bf0a9c06080b5675fadb97
Shahrad <shahradbagheri6@gmail.com>
2023-10-12 14:52:05 +0330

    created and implemented UserServiceImpl

---
+++ src/main/java/service/Impl/UserServiceImpl.java
@@ -0,0 +1,35 @@
+package service.Impl;
+
+import model.User;
+import repository.UserRepository;
+import service.UserService;
+import util.ApplicationContext;
+import util.EntityManagerSingleton;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.PersistenceException;
+import java.util.Objects;
+
+public class UserServiceImpl implements UserService {
+
+    EntityManager entityManager = EntityManagerSingleton.getInstanceEM();
+    UserRepository userRepository = ApplicationContext.userRepository;
+
+    @Override
+    public User signIn(String username, String password) {
+        EntityTransaction entityTransaction = entityManager.getTransaction();
+        try {
+            entityTransaction.begin();
+
+            User user = userRepository.findByUsername(username);
+            if (Objects.equals(user.getPassword(), password)) {
+                entityTransaction.commit();
+                return user;
+            }
+        } catch (PersistenceException | IllegalStateException e) {
+            entityTransaction.rollback();
+        }
+        return null;
+    }
+}
