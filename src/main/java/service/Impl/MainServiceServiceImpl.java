6114b864e8000b96d32ccd74e9d816ee39c4cf4c
Shahrad <shahradbagheri6@gmail.com>
2023-10-12 15:38:30 +0330

    implemented the create method

--- src/main/java/service/Impl/MainServiceServiceImpl.java
+++ src/main/java/service/Impl/MainServiceServiceImpl.java
@@ -1,15 +1,38 @@
 package service.Impl;
 
 import model.MainService;
+import repository.MainServiceRepository;
 import service.MainServiceService;
+import util.ApplicationContext;
+import util.EntityManagerSingleton;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.PersistenceException;
 import java.util.List;
 
 public class MainServiceServiceImpl implements MainServiceService {
 
+    private final EntityManager entityManager = EntityManagerSingleton.getInstanceEM();
+    private final MainServiceRepository mainServiceRepository = ApplicationContext.mainServiceRepository;
+
     @Override
     public MainService create(MainService mainService) {
-        return null;
+
+        EntityTransaction entityTransaction = entityManager.getTransaction();
+
+        try {
+            entityTransaction.begin();
+
+            mainServiceRepository.create(mainService);
+
+            entityTransaction.commit();
+            return mainService;
+        } catch (PersistenceException | IllegalStateException e) {
+
+            entityTransaction.rollback();
+            return null;
+        }
     }
 
     @Override
