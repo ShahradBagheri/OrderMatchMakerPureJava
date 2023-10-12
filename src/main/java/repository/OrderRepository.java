448c1741ab7619bf7d581c09ef6b370f96154b92
Shahrad <shahradbagheri6@gmail.com>
2023-10-12 08:10:52 +0330

    added methods to OrderRepository

--- src/main/java/repository/OrderRepository.java
+++ src/main/java/repository/OrderRepository.java
@@ -1,4 +1,18 @@
 package repository;
 
+import model.Order;
+
+import java.util.List;
+
 public interface OrderRepository {
+
+    Order create(Order order);
+
+    Order update(Order order);
+
+    void delete(Order order);
+
+    Order findById(Long id);
+
+    List<Order> findAll();
 }
